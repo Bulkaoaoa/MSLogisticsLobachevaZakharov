deedc7e007e7f621eac81e61c4154e82cb676346
wtauougtdccl@dropmail.me <wtauougtdccl@dropmail.me>
2021-01-22 19:04:01 +0300

    Добавляю классы для апи

---
+++ Mob/Mob/Mob/Classes/Rule.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mob.Classes
+{
+    public class Rule
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Mob/Mob/Mob/Classes/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mob.Classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public object Patronymic { get; set; }
+        public object Photo { get; set; }
+        public object Telephone { get; set; }
+        public object IsFree { get; set; }
+        public object ScheduleCourier { get; set; }
+
+    }
+
+
+}
