298cbf28aade02c8e1c61081cad8237fe7559719
Данила Захаров <dtnchik.zaharow@gmail.com>
2021-01-26 02:24:35 +0300

    кое какие стили

--- desctop/Desctop/MainWindow.xaml.cs
+++ desctop/Desctop/MainWindow.xaml.cs
@@ -28,5 +28,27 @@ namespace Desctop
             AppData.MainFrame = MainFrame;
             AppData.MainFrame.Navigate(new AutorizationPage());
         }
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            var page = AppData.MainFrame.Content as Page;
+            if (page.Title == "Авторизация")
+            {
+                BtnBack.IsEnabled = false;
+                BtnBack.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BtnBack.IsEnabled = true;
+                BtnBack.Visibility = Visibility.Visible;
+            }
+            Title = $"{page.Title}";
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (AppData.MainFrame.CanGoBack)
+                AppData.MainFrame.GoBack();
+        }
     }
 }
--- desctop/Desctop/Pages/AutorizationPage.xaml.cs
+++ desctop/Desctop/Pages/AutorizationPage.xaml.cs
@@ -29,6 +29,16 @@ namespace Desctop.Pages
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
+            string error = "";
+            if (String.IsNullOrEmpty(TbLogin.Text))
+                error += "Введите логин\n";
+            if (String.IsNullOrEmpty(PbPassword.Password))
+                error += "• Введите пароль\n";
+            if (!String.IsNullOrWhiteSpace(error))
+            {
+                MessageBox.Show(error, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
             var userList = AppData.Context.User.ToList();
             User user = userList.FirstOrDefault(p => p.Login == TbLogin.Text && p.Password == PbPassword.Password);
             if (user != null)
@@ -36,15 +46,21 @@ namespace Desctop.Pages
                 switch (user.RoleId)
                 {
                     case 1:
+                        MessageBox.Show("Для клиента используйте мобильное приложение", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                         break;
                     case 2:
                         AppData.MainFrame.Navigate(new ManagerPage());
                         break;
                     case 3:
+                        MessageBox.Show("Для курьера используйте мобильное приложение", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
                         break;
                     default:
                         break;
                 }
+            }
+            else
+            {
+                MessageBox.Show("Не верный логин или пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
